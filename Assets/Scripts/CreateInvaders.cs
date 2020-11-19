31d1c21654ae23e2deb4071a6af88fe06302b2dc
zyy55 <Woaiwodejia2018>
2020-11-19 20:32:06 +0100

    Añadir algo en cada carpeta para que se aparezca las carpetas

---
+++ Assets/Scripts/CreateInvaders.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateInvaders : MonoBehaviour
+{
+    public GameObject invader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int j = 0; j < 5; j++)
+            for (int i = 0; i < 10; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(invader, new Vector3(i - 4.5f, 0.75f * j + 1.2f, 0.0f), transform.rotation);
+                obj.transform.parent = transform;
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
