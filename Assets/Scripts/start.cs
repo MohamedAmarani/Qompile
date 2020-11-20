ab3f82da64cc7e0a781a56eb72af8849e98eab56
zyy55 <Woaiwodejia2018>
2020-11-20 23:28:25 +0100

    menu principal diseño, cuando pulsa play, va a una escena nueva

---
+++ Assets/Scripts/start.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void CarregaEscena(string nom)
+    {
+        SceneManager.LoadScene(nom);
+        Time.timeScale = 1;
+    }
+
+}
