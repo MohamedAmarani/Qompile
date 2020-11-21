2997fda593305325c36359dd4e3517dea544866f
zyy55 <Woaiwodejia2018>
2020-11-21 11:35:15 +0100

    añadir pausa a la escena2(image disable)

---
+++ Assets/Scripts/pausa.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class pausa : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+        }
+    }
+    public void Continual()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void CarregaEscena(string nom)
+    {
+        SceneManager.LoadScene(nom);
+        Time.timeScale = 1;
+    }
+
+}
