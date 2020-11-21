55603be42efe7ec26c73d7a377861960d7239117
zyy55 <Woaiwodejia2018>
2020-11-21 13:24:06 +0100

    Mayoria de animaciones hecho

---
+++ Assets/Scripts/playAgain.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playAgain : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+    public void CarregaEscena(string nom)
+    {
+        SceneManager.LoadScene(nom);
+        Time.timeScale = 1;
+    }
+    public void Clicar()
+    {
+
+    }
+}
