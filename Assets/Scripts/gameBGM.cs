21f2503ff522e53c70d604fb23ed57cf4107cf9a
zyy55 <zyuanyu998@gmail.com>
2020-12-04 19:46:52 +0100

    musica bgm hecho

---
+++ Assets/Scripts/gameBGM.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameBGM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject player = GameObject.Find("Audio Source");
+        if (player)
+            Destroy(player);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
