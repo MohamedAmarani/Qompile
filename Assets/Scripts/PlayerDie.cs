c41cea3622bb41b04f2094868a5f4ea27b845d63
zyy55 <zyuanyu998@gmail.com>
2020-12-08 16:21:27 +0100

    añadir particle cuando muere la pilota, volver a cargar la escena después de 5 segundos.

---
+++ Assets/Scripts/PlayerDie.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayerDie : MonoBehaviour
+{
+    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool encuentra = transform.Find("Ball");
+        Debug.Log(encuentra);
+        if (encuentra == false)
+        {
+            Invoke("VolverCarregaEscena", 5f);
+        }
+    }
+
+    public void VolverCarregaEscena()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+    }
+}
